985a607635871d357f0b291f60196b22ab47e572
Naka <e1q22056@oit.ac.jp>
2025-05-23 17:14:13 +0900

    first

---
+++ yolo11m.py
@@ -0,0 +1,33 @@
+import cv2
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolo11m.pt")
+
+cap = cv2.VideoCapture(0)
+# Perform object detection on an image
+
+if not  cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+while True:
+    ret,frame = cap.read()
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+
+    results = model(frame)
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("YOLO Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+#results = model("https://ultralytics.com/images/bus.jpg")
+#results[0].show()
